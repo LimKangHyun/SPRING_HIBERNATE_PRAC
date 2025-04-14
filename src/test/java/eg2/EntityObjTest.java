4ba101a3afabf85a1837bde543bbf72c0a0c6109
limkanghyun <limkh06910691@gmail.com>
2025-04-15 02:23:20 +0900

    feat: GymMemberShip 엔티티에 Level Enum 테스트 추가 - GOLD, SILVER, BRONZE 등급의 GymMemberShip 객체 저장 테스트

--- src/test/java/eg2/EntityObjTest.java
+++ src/test/java/eg2/EntityObjTest.java
@@ -1,9 +1,14 @@
 package eg2;
 
+import domain.eg2._3.GymMemberShip;
+import domain.eg2._3.Level;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import org.junit.jupiter.api.*;
+import util.TestUtil;
+
+import static util.TestUtil.*;
 
 public class EntityObjTest {
 
@@ -32,5 +37,27 @@ public class EntityObjTest {
     @DisplayName("table strategy test")
     void table_stg_test() throws Exception {
 
+    }
+
+    @Test
+    @DisplayName("enum test")
+    void enum_test() throws Exception {
+
+        executeCommit(entityManager, () -> {
+
+            GymMemberShip memberShip1 = new GymMemberShip();
+            memberShip1.setMemberShipLevel(Level.GOLD);
+
+            GymMemberShip memberShip2 = new GymMemberShip();
+            memberShip2.setMemberShipLevel(Level.SILVER);
+
+            GymMemberShip memberShip3 = new GymMemberShip();
+            memberShip3.setMemberShipLevel(Level.BRONZE);
+
+            entityManager.persist(memberShip1);
+            entityManager.persist(memberShip2);
+            entityManager.persist(memberShip3);
+            return null;
+        });
     }
 }
