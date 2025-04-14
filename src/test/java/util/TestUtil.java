c052477ecc79d126771254d6ccb2e7559555dc68
limkanghyun <limkh06910691@gmail.com>
2025-04-15 00:13:28 +0900

    feat: executeCommit 트랜잭션 유틸과 genNumStr 랜덤 숫자 문자열 생성기 추가

---
+++ src/test/java/util/TestUtil.java
@@ -0,0 +1,31 @@
+package util;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TestUtil {
+
+    public static String genNumStr() {
+        int number = (int) (Math.random() * 10000);
+        return String.valueOf(number);
+    }
+
+    public static void executeCommit(EntityManager entityManager, Runnable action) {
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        try {
+            // 트랜잭션 시작
+            transaction.begin();
+            // 비즈니스 로직 실행
+            action.run();
+            transaction.commit();
+        } catch (Exception e) {
+            // 트랜잭션이 활성상태인경우인지 확인 후 롤백, 비활상태라면 롤백하면 예외 터지고 의미 없는 동작이므로 예외 처리
+            if (transaction.isActive()) transaction.rollback();
+            log.error(e.getMessage(), e);
+            e.printStackTrace();
+        }
+    }
+}
