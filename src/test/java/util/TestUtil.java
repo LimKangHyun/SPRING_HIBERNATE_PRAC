50ad666f8a05943d90fd3a8db2e95ff19cc3cc59
limkanghyun <limkh06910691@gmail.com>
2025-04-15 01:14:35 +0900

    refactor: executeCommit 메서드 반환 타입을 void에서 제네릭 타입으로 변경
    - executeCommit 메서드의 반환 타입을 void에서 제네릭 타입(T)으로 수정하여 트랜잭션 액션의 결과를 반환할 수 있게 변경, 다양한 트랜잭션 타입 처리 가능

--- src/test/java/util/TestUtil.java
+++ src/test/java/util/TestUtil.java
@@ -12,20 +12,27 @@ public class TestUtil {
         return String.valueOf(number);
     }
 
-    public static void executeCommit(EntityManager entityManager, Runnable action) {
+    public static <T> T executeCommit(EntityManager entityManager, TransactionalAction<T> action) {
         EntityTransaction transaction = entityManager.getTransaction();
 
         try {
             // 트랜잭션 시작
             transaction.begin();
             // 비즈니스 로직 실행
-            action.run();
+            T result = action.execute();
             transaction.commit();
+            return result;
         } catch (Exception e) {
             // 트랜잭션이 활성상태인경우인지 확인 후 롤백, 비활상태라면 롤백하면 예외 터지고 의미 없는 동작이므로 예외 처리
             if (transaction.isActive()) transaction.rollback();
             log.error(e.getMessage(), e);
             e.printStackTrace();
+            return null;
         }
+    }
+
+    @FunctionalInterface
+    public interface TransactionalAction<T> {
+        T execute() throws Exception;
     }
 }
