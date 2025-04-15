5c895810165d8123316b038737de59dc98dfe7c6
limkanghyun <limkh06910691@gmail.com>
2025-04-16 00:24:15 +0900

    feat: Team, Player 예시 쿼리 추가

---
+++ sql/schema.sql
@@ -0,0 +1,12 @@
+INSERT INTO teams (team_id, name) VALUES (1, 'FC Example');
+
+INSERT INTO players (name, team_id) VALUES ('Lionel Messi', 1);
+INSERT INTO players (name, team_id) VALUES ('Cristiano Ronaldo', 1);
+INSERT INTO players (name, team_id) VALUES ('Kylian Mbappe', 1);
+INSERT INTO players (name, team_id) VALUES ('Neymar Jr', 1);
+INSERT INTO players (name, team_id) VALUES ('Erling Haaland', 1);
+INSERT INTO players (name, team_id) VALUES ('Luka Modric', 1);
+INSERT INTO players (name, team_id) VALUES ('Kevin De Bruyne', 1);
+INSERT INTO players (name, team_id) VALUES ('Virgil van Dijk', 1);
+INSERT INTO players (name, team_id) VALUES ('Sergio Ramos', 1);
+INSERT INTO players (name, team_id) VALUES ('Robert Lewandowski', 1);
