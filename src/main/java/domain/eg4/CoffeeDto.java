7cbacf41cb9eeb01832ba03403b32101f7623500
limkanghyun <limkh06910691@gmail.com>
2025-04-16 01:52:50 +0900

    feat: Coffee DTO 생성

---
+++ src/main/java/domain/eg4/CoffeeDto.java
@@ -0,0 +1,14 @@
+package domain.eg4;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CoffeeDto {
+
+    private String name;
+    private Integer price;
+}
